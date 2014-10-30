4638210ca9c5cf14deb1fa50d9c192480e35ecb1
fliz <flizarthanon@gmail.com>
2014-10-30 14:16:44 -0700

    build user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ActiveRecord::Base
+
+    #this is just for the validation, not persistent.
+	attr_reader :password
+
+	validates :email, :session_token, presence: true
+	validates :email, uniqueness: true
+	validates :password_digest, presence: { message: "Password can't be blank"}
+	validates :password, length: { minimum: 6, allow_nil: true }
+	after_initialize :ensure_session_token
+
+	def self.generate_session_token
+		SecureRandom::urlsafe_base64(16)
+	end
+
+	def reset_session_token!
+		self.session_token = self.class.generate_session_token
+		self.save!
+		self.session_token
+	end
+
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def is_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
+	def self.find_by_credentials(email, password)
+		user = User.find_by(email: email)
+		user.try(:is_password?, password) ? user : nil 
+	end
+
+	private
+
+	def ensure_session_token
+		self.session_token ||= self.class.generate_session_token
+	end
+
+
+end
